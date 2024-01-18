90de848b68c1b6da93c7232af373d78b5e86a2bc
JisuJung928 <wjdwltn928@snu.ac.kr>
2024-01-18 11:28:20 +0900

    Update version dependency

--- cdvae/run.py
+++ cdvae/run.py
@@ -14,7 +14,6 @@ from pytorch_lightning.callbacks import (
     LearningRateMonitor,
     ModelCheckpoint,
 )
-from pytorch_lightning.loggers import WandbLogger
 
 from cdvae.common.utils import log_hyperparameters, PROJECT_ROOT
 
@@ -77,9 +76,6 @@ def run(cfg: DictConfig) -> None:
         cfg.data.datamodule.num_workers.val = 0
         cfg.data.datamodule.num_workers.test = 0
 
-        # Switch wandb mode to offline to prevent online logging
-        cfg.logging.wandb.mode = "offline"
-
     # Hydra run directory
     hydra_dir = Path(HydraConfig.get().run.dir)
 
@@ -108,22 +104,6 @@ def run(cfg: DictConfig) -> None:
     # Instantiate the callbacks
     callbacks: List[Callback] = build_callbacks(cfg=cfg)
 
-    # Logger instantiation/configuration
-    wandb_logger = None
-    if "wandb" in cfg.logging:
-        hydra.utils.log.info("Instantiating <WandbLogger>")
-        wandb_config = cfg.logging.wandb
-        wandb_logger = WandbLogger(
-            **wandb_config,
-            tags=cfg.core.tags,
-        )
-        hydra.utils.log.info("W&B is now watching <{cfg.logging.wandb_watch.log}>!")
-        wandb_logger.watch(
-            model,
-            log=cfg.logging.wandb_watch.log,
-            log_freq=cfg.logging.wandb_watch.log_freq,
-        )
-
     # Store the YaML config separately into the wandb dir
     yaml_conf: str = OmegaConf.to_yaml(cfg=cfg)
     (hydra_dir / "hparams.yaml").write_text(yaml_conf)
@@ -140,7 +120,6 @@ def run(cfg: DictConfig) -> None:
     hydra.utils.log.info("Instantiating the Trainer")
     trainer = pl.Trainer(
         default_root_dir=hydra_dir,
-        logger=wandb_logger,
         callbacks=callbacks,
         deterministic=cfg.train.deterministic,
         check_val_every_n_epoch=cfg.logging.val_check_interval,
@@ -155,10 +134,6 @@ def run(cfg: DictConfig) -> None:
 
     hydra.utils.log.info("Starting testing!")
     trainer.test(datamodule=datamodule)
-
-    # Logger closing to release resources/avoid multi-run conflicts
-    if wandb_logger is not None:
-        wandb_logger.experiment.finish()
 
 
 @hydra.main(config_path=str(PROJECT_ROOT / "conf"), config_name="default")
